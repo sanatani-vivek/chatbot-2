b9fd48fecd8d7f58dff2c9f35b932b4a37d7ebb5
Santanivivek <uzumakivivek.1@gmail.com>
2026-08-01 16:35:38 +0530

    Fix root route to serve public folder

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path'); 
 
 app.use(express.json());
-app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 function getBotResponse(msg) {
     msg = msg.toLowerCase();
